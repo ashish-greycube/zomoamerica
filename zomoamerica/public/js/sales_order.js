eedc5d823af9d86a6770a3e99617cf41b1e1532c
ashish-greycube <mr.ashish.shah@gmail.com>
2019-12-14 19:01:10 +0530

    remove scanner code changes in so

--- zomoamerica/public/js/sales_order.js
+++ zomoamerica/public/js/sales_order.js
@@ -1,13 +1,13 @@
-frappe.ui.form.on('Sales Order', {
-	scan_barcode(frm, e) {
-		$(window).on('keydown', function (e) {
-			var key = frappe.ui.keys.get_key(e);
-			if (key == 'enter') {
-				cur_frm.refresh_field('items')
-			}
-		});
-	}
-})
+// frappe.ui.form.on('Sales Order', {
+// 	scan_barcode(frm, e) {
+// 		$(window).on('keydown', function (e) {
+// 			var key = frappe.ui.keys.get_key(e);
+// 			if (key == 'enter') {
+// 				cur_frm.refresh_field('items')
+// 			}
+// 		});
+// 	}
+// })
 
 frappe.ui.form.on('Sales Order Item', {
 	item_code(frm, cdt, cdn) {
@@ -16,13 +16,13 @@ frappe.ui.form.on('Sales Order Item', {
             row.uom='';
             refresh_field("uom", cdn, "items");
         }
-		frappe.db.get_value('Item', row.item_code, 'stock_uom')
-			.then(r => {
-				if (r.message.stock_uom) {
-					let stock_uom = r.message.stock_uom
-					row.stock_uom = stock_uom
-					cur_frm.add_child("items");
-				}
-			})
+		// frappe.db.get_value('Item', row.item_code, 'stock_uom')
+		// 	.then(r => {
+		// 		if (r.message.stock_uom) {
+		// 			let stock_uom = r.message.stock_uom
+		// 			row.stock_uom = stock_uom
+		// 			cur_frm.add_child("items");
+		// 		}
+		// 	})
 	}
 })
