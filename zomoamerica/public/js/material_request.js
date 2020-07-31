ec60c63e887d68c8c23b98efd5d7c264c4fdf2db
ashish-greycube <mr.ashish.shah@gmail.com>
2020-07-31 08:41:11 +0530

    add amount

--- zomoamerica/public/js/material_request.js
+++ zomoamerica/public/js/material_request.js
@@ -2,12 +2,16 @@ frappe.ui.form.on("Material Request",{
     validate: function(frm){
         if (frm.doc.material_request_type =='Withdrawal Request') {
             let total_qty_cf=0;
+            let total_amount_cf=0;
+            frappe.defaults.get_default("currency")
             frm.doc.items.forEach(item=> {
                 total_qty_cf+=item.qty
+                total_amount_cf+=item.amount
             })
             frm.set_value("total_qty_cf",total_qty_cf);
             frm.set_value("net_weight_cf",total_qty_cf*6);
-            frm.set_value("gross_weight_cf",total_qty_cf*6*1.225);            
+            frm.set_value("gross_weight_cf",total_qty_cf*6*1.225); 
+            frm.set_value("total_amount_cf",total_amount_cf);           
         }
 
     }
