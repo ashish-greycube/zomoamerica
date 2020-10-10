defd2059729f73250877613ecd31c756e8ca6ff1
jigneshpshah <jignesh@greycube.in>
2020-10-10 04:24:46 +0000

    schedule i updated for pr lcv

--- zomoamerica/zomoamerica/doctype/tobacco_legal_compliance/tobacco_legal_compliance.py
+++ zomoamerica/zomoamerica/doctype/tobacco_legal_compliance/tobacco_legal_compliance.py
@@ -343,17 +343,19 @@ class TobaccoLegalCompliance(Document):
                 where
                     parent_item_group = 'TOBACCO'))
             ) as PRA,
-            ( SELECT sum(COALESCE(tlctc.amount,0)) as totalcost
+            (SELECT sum(COALESCE(tlctc.amount,0)) as totalcost
             FROM `tabLanded Cost Voucher` tlcv 
             INNER JOIN `tabLanded Cost Purchase Receipt`tlcpr on tlcv.name = tlcpr.parent
             and tlcpr.receipt_document_type = 'Purchase Receipt' and tlcpr.receipt_document
             in
-            (SELECT distinct name from `tabPurchase Receipt` PR  where 
+            (SELECT distinct PR.name from `tabPurchase Receipt` PR  
+            INNER JOIN tabSupplier  SR ON PR.supplier = SR.name AND SR.country  <> 'United States'
+            where 
             PR.docstatus = 1
-            and PR.set_warehouse = %s  
-            and MONTHNAME(PR.posting_date) =   %s  
-            and year(PR.posting_date) = %s  
-            and name in  (select distinct parent from  `tabPurchase Receipt Item` PRI 
+            and PR.set_warehouse = %s
+            and MONTHNAME(PR.posting_date) =  %s
+            and year(PR.posting_date) = %s
+            and PR.name in  (select distinct PRI.parent from  `tabPurchase Receipt Item` PRI 
             where PRI.item_group in (
                 select
                     distinct name
@@ -610,7 +612,7 @@ class TobaccoLegalCompliance(Document):
             tlc.legal_company as 'TaxpayerName',
             TRIM(tlc.address_line1) as 'Address',
             'PATERSON NJ 07053' as 'CityStateZip',
-            (COALESCE(MTA.mt_total_amt,0)+ COALESCE(PRA.p_total,0)) AS 2A_Total
+            (COALESCE(MTA.mt_total_amt,0)+ COALESCE(PRA.p_total,0) + COALESCE(PRLCV.totalcost,0))AS 2A_Total
             from `tabTobacco Legal Compliance` AS tlc ,
             (select  SUM(total_amount)   as mt_total_amt
             from `tabStock Entry` as SE 
@@ -645,8 +647,34 @@ class TobaccoLegalCompliance(Document):
                     `tabItem Group`
                 where
                     parent_item_group = 'TOBACCO'))
-            ) as PRA
-            where tlc.name = %s""", (self.company_warehouse, self.month, self.year, self.company_warehouse, self.month, self.year, self.name), as_dict=True)
+            ) as PRA,
+            ( SELECT sum(COALESCE(tlctc.amount,0)) as totalcost
+            FROM `tabLanded Cost Voucher` tlcv 
+            INNER JOIN `tabLanded Cost Purchase Receipt`tlcpr on tlcv.name = tlcpr.parent
+            and tlcpr.receipt_document_type = 'Purchase Receipt' and tlcpr.receipt_document
+            in
+            (SELECT distinct PR.name from `tabPurchase Receipt` PR  
+            INNER JOIN tabSupplier  SR ON PR.supplier = SR.name AND SR.country  <> 'United States'
+            where 
+            PR.docstatus = 1
+            and PR.set_warehouse = %s
+            and MONTHNAME(PR.posting_date) =  %s
+            and year(PR.posting_date) = %s
+            and PR.name in  (select distinct PRI.parent from  `tabPurchase Receipt Item` PRI 
+            where PRI.item_group in (
+                select
+                    distinct name
+                from
+                    `tabItem Group`
+                where
+                    parent_item_group = 'TOBACCO'
+            )))
+            INNER JOIN `tabLanded Cost Taxes and Charges` tlctc on tlcv.name = tlctc.parent
+            and tlctc.parenttype = "Landed Cost Voucher" 
+            INNER JOIN  tabAccount  as ac on 
+            tlctc.expense_account =  ac.name 
+            and ac.account_type='Tax') as PRLCV
+            where tlc.name = %s""", (self.company_warehouse, self.month, self.year, self.company_warehouse, self.month, self.year,self.company_warehouse, self.month, self.year, self.name), as_dict=True)
         return field_dictionary and field_dictionary[0] or {}
 
     def month_converter(self,month):
