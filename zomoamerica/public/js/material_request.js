34f5019e447872b79df6fbb0799f6c7916d094db
ashish-greycube <mr.ashish.shah@gmail.com>
2020-10-31 09:27:10 +0530

    bug : total_amount_cf in material request

--- zomoamerica/public/js/material_request.js
+++ zomoamerica/public/js/material_request.js
@@ -4,10 +4,11 @@ frappe.ui.form.on("Material Request",{
             let total_qty_cf=0;
             let total_amount_cf=0;
             frappe.defaults.get_default("currency")
+            let mastercase_rate_cf = frm.doc.mastercase_rate_cf
             frm.doc.items.forEach(item=> {
                 total_qty_cf+=item.qty
-                total_amount_cf+=item.amount
             })
+            total_amount_cf = total_qty_cf * mastercase_rate_cf
             frm.set_value("total_qty_cf",total_qty_cf);
             frm.set_value("net_weight_cf",total_qty_cf*6);
             frm.set_value("gross_weight_cf",total_qty_cf*6*1.225); 
